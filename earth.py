7b7ffc3220752b4abbb3110be1cfea991272b4ce
Austral-marcos <mbrunettaalvarezper@mail.austral.edu.ar>
2025-03-28 12:00:10 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,4 @@
 def earth():
-    x = "Bangladesh"
-    y = "Barbados"
+    X, Y = "Bangladesh", "Barbados"
+    if X < Y:
+      print(f"The result of X comes first in the dictionary than Y is {X < Y}.")
