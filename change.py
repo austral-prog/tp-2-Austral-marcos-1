cd3a3ec8f45c25508603dbf90eabc6d266d83cb2
Austral-marcos <mbrunettaalvarezper@mail.austral.edu.ar>
2025-03-28 12:02:54 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,18 @@
 def change():
-    expense = 23.75
-    money = 100
+    gasto_persona = 23.75
+    pago = 100
+    x = "Ingresar gasto:"
+    y = "Dinero recibido"
+    z = pago - gasto_persona
+    l = str(z).split('.')[1]
+    print(x)
+    print(gasto_persona)
+    print(y)
+    print(pago)
+    print("\n")
+    print("Vuelto")
+    print("\n")
+    print("Pesos:")
+    print(z)
+    print("Centavos:")
+    print(l)
