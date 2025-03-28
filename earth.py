cec85b141ab030deb21f90950d4d45bd82647320
Austral-marcos <mbrunettaalvarezper@mail.austral.edu.ar>
2025-03-28 12:36:42 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -2,4 +2,6 @@ def earth():
     X, Y = "Bangladesh", "Barbados"
     if X < Y:
       print(f"The result of X comes first in the dictionary than Y is {X < Y}.")
+    if X < Y:
+        print(f"The result of Y comes first in the dictionary than X is {X < Y}.")
 earth()
