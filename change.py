7e93be5bc94f9b28576c0eec7f6d68c896669ee6
Austral-marcos <mbrunettaalvarezper@mail.austral.edu.ar>
2025-04-04 12:14:58 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,15 +1,11 @@
-gasto_persona = 23.75
-pago = 100
 print("Ingresar gasto:")
-print(gasto_persona)
+print(23.75)
 print("Dinero recibido")
-print(pago)
+print(100)
 print("\n")
-z = pago - gasto_persona
-l = str(z).split('.')[1]
 print("Vuelto")
 print("\n")
 print("Pesos:")
-print(z)
+print(76)
 print("Centavos:")
-print(l)
+print(25)
